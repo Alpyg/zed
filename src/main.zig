0b3e36986078cf41f2820449ae8f6c2495383d8b
Alpyg <alpyg@pm.me>
2025-02-19 17:10:47 -0500

    q quitting

--- src/main.zig
+++ src/main.zig
@@ -3,6 +3,6 @@ const lib = @import("zed_lib");
 
 pub fn main() !u8 {
     var c: [1]u8 = .{0};
-    while (std.io.getStdIn().read(&c) catch unreachable == 1) {}
+    while (std.io.getStdIn().read(&c) catch unreachable == 1 and c[0] != 'q') {}
     return 0;
 }
